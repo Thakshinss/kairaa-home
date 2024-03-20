288c94a07f89c364aef4088506a43d44fba1b053
thakshin <thadkshinss001@gmail.com>
2024-03-20 16:07:58 +0530

    updated course image

--- app/(pages)/courses/[id]/page.jsx
+++ app/(pages)/courses/[id]/page.jsx
@@ -1,6 +1,7 @@
 "use client"
 
 import CourseContent from '@/app/components/CourseContent'
+import { BadgeCheck, Clock, Languages, NotebookPen, Puzzle, ShieldCheck, TriangleRight } from 'lucide-react'
 import Image from 'next/image'
 import React, { useState,useEffect } from 'react'
 
@@ -19,15 +20,15 @@ function CourseDetails({params}) {
       })
   }, [id])
   const [course, setCourse] = useState([])
-  // console.log(course.topics[2].name);
+  // console.log(course.topics);
   // console.log(courses[1].topics[1].content[1]);
   
 
   return (
     <section>
       <div >
-        <div className='justify-around pt-5 pb-5 md:flex px-7'>
-          <div className='w-1/2'>
+        <div className='pt-5 pb-5 justify-evenly md:flex px-7'>
+          <div className='md:w-1/2'>
             <Image
               src={course.img}
               width={500}
@@ -37,49 +38,51 @@ function CourseDetails({params}) {
               <p className='pt-4'>{course.description}</p>
           </div>
 
-          <div className='w-1/2 px-8'>
-              <h1>Course Features</h1>
-              <div>
+          <div className='px-8 pt-3 md:border-l-2 md:w-1/2 md:pt-0'>
+              <h1 className='pb-3 text-2xl font-bold'>Course Features</h1>
+              <div className='pt-2'>
                 <div>
-                  <div className='flex justify-between'>
-                    <p>Lectures</p>
-                    <p>34</p>
+                  <div className='flex justify-between pb-2'>
+                  
+                    <p><NotebookPen className='inline-block mr-1 text-blue-800' />Lectures</p>
+                    <p>22</p>
                   </div>
                 </div>
                 <div>
-                  <div className='flex justify-between'>
-                    <p>Lectures</p>
-                    <p>34</p>
+                  <div className='flex justify-between pb-2'>
+                    <p><Puzzle className='inline-block mr-1 text-blue-800' />Quizzes</p>
+                    <p>1</p>
                   </div>
                 </div>
                 <div>
-                  <div className='flex justify-between'>
-                    <p>Lectures</p>
-                    <p>34</p>
+                  <div className='flex justify-between pb-2'>
+                    <p><Clock className='inline-block mr-1 text-blue-800' />Duration</p>
+                    <p>1</p>
+                  </div>
+                </div>
+                
+                <div>
+                  <div className='flex justify-between pb-2'>
+                    <p> <TriangleRight className='inline-block mr-1 text-blue-800' />Skill level</p>
+                    <p>All levels</p>
                   </div>
                 </div>
                 <div>
-                  <div className='flex justify-between'>
-                    <p>Lectures</p>
-                    <p>34</p>
+                  <div className='flex justify-between pb-2'>
+                    <p> <Languages className='inline-block mr-1 text-blue-800' />Language</p>
+                    <p>English</p>
                   </div>
                 </div>
                 <div>
-                  <div className='flex justify-between'>
-                    <p>Lectures</p>
-                    <p>34</p>
+                  <div className='flex justify-between pb-2'>
+                    <p><ShieldCheck className='inline-block mr-1 text-blue-800' />Certificate</p>
+                    <p>Yes</p>
                   </div>
                 </div>
                 <div>
-                  <div className='flex justify-between'>
-                    <p>Lectures</p>
-                    <p>34</p>
-                  </div>
-                </div>
-                <div>
-                  <div className='flex justify-between'>
-                    <p>Lectures</p>
-                    <p>34</p>
+                  <div className='flex justify-between pb-2'>
+                    <p> <BadgeCheck className='inline-block mr-1 text-blue-800' />Assessments</p>
+                    <p>Self</p>
                   </div>
                 </div>
                 
@@ -97,6 +100,7 @@ function CourseDetails({params}) {
               // ))
           ))
         }
+        
 
       </div>
     </section>
