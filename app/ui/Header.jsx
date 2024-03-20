fc901a07d0cd89b0863b2f7c2d9d7bd585d0fa9b
thakshin <thadkshinss001@gmail.com>
2024-03-20 19:08:49 +0530

    updated home img

--- app/ui/Header.jsx
+++ app/ui/Header.jsx
@@ -16,7 +16,7 @@ function Header() {
         <div className='p-3 rounded-lg'>
           <Image 
           className='hidden rounded-lg md:flex'
-            src="/home2.jpeg"
+            src="/homee.png"
             width={400}
             height={300}
             alt="Picture of the author"/>
